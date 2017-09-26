40b7f4879283cb1ca876611c0adc6e354adcbec4
Robin-Hoodie <robin.hellemans@fourcast.io>
2017-09-26 11:06:02 +0200

    First commit

---
+++ src/test/java/com/example/appengine/test/gettingstartedjava/helloworld/HelloServletTest.java
@@ -0,0 +1,13 @@
+package com.example.appengine.test.gettingstartedjava.helloworld;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloServletTest {
+
+  @Test
+  public void test() {
+    assertEquals(1 + 1, 2);
+  }
+}
